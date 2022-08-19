042658b2c3f9a7c7145e29deb2ece070c2d79d9b
Алексей Ипполитов <setgecko@gmail.com>
2022-08-19 13:54:44 +0300

    Попробовал перечисления

--- Enumeration/Program.cs
+++ Enumeration/Program.cs
@@ -10,6 +10,24 @@ namespace Enumeration
     {
         static void Main(string[] args)
         {
+            DaysOfWeek MyFavoriteDay;
+            MyFavoriteDay = DaysOfWeek.Friday;
+            Console.WriteLine(MyFavoriteDay);
+
+            Console.ReadLine();
+        }
+        enum DaysOfWeek : byte
+        {
+            Tuesday,
+            Monday,
+            Wednesday,
+            Friday
+        }
+        enum Semaphore
+        {
+            Red = 100,
+            Yellow = 200,
+            Green = 300
         }
     }
 }
