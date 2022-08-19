2db67394563a41920dcd680aa19bb20ef7814ba1
Алексей Ипполитов <setgecko@gmail.com>
2022-08-19 13:14:57 +0300

    Попробовал основные типы данных

--- MainDataTypes/Program.cs
+++ MainDataTypes/Program.cs
@@ -10,6 +10,20 @@ namespace MainDataTypes
     {
         static void Main(string[] args)
         {
+            string MyName = "Алексей";
+            byte MyAge = 35;
+            bool HaveIPet = true;
+            double MyShoesSize = 41.0;
+
+            Console.WriteLine("Меня зовут " + MyName);
+            Console.WriteLine("Мой возраст " + MyAge + " лет");
+            Console.WriteLine("У меня есть животное? " + HaveIPet);
+            Console.WriteLine("Мой размер ноги - " + MyShoesSize);
+
+            Console.WriteLine("Минимальное значение типа Int равно {0}", int.MinValue);
+            Console.WriteLine("Максимальное значение типа Int равно {0}", int.MaxValue);
+
+            Console.ReadLine();
         }
     }
 }
