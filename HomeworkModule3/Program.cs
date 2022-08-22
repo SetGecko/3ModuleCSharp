339e8095a6b3dcf23cbdfe7041a52e064474ab3e
Алексей Ипполитов <setgecko@gmail.com>
2022-08-22 17:01:29 +0300

    Создал HomeworkModule3 - написал программу из ДЗ

---
+++ HomeworkModule3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkModule3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваше имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Сколько Вам лет? ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вас зовут {0} и Вам {1} лет. Очень приятно!", name, age);
+            Console.WriteLine("Введите Вашу дату рождения: ");
+            var birthdate = Console.ReadLine();
+            Console.WriteLine("Дата Вашего дня рождения: {0}", birthdate);
+            Console.ReadKey();
+        }
+    }
+}
--- TypeConversion/Program.cs
+++ TypeConversion/Program.cs
@@ -10,6 +10,28 @@ namespace TypeConversion
     {
         static void Main(string[] args)
         {
+            int olddata = 6;
+            string data = olddata.ToString();
+            Console.WriteLine(data);
+            Console.WriteLine(olddata);
+            Console.WriteLine();
+
+            Console.WriteLine("Введите ваш возраст: ");
+            int age;
+            bool iscorrect = int.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine("Вам {0} лет", age);
+            Console.WriteLine();
+
+            Console.WriteLine("Введите ваше имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите ваш возраст: ");
+            byte age2 = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine("Вас зовут {0}, вам {1} лет", name, age2);
+            Console.WriteLine();
+
+            Console.WriteLine("Какой ваш любимый день недели?");
+            DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
+            Console.WriteLine("Ваш любимый день недели - {0}", day);
 
 
             Console.ReadKey();
